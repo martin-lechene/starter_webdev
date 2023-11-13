6d8bb73c8df8c96ebb74accbea3062b08450e899
Martin Lechêne <martin@lechene.be>
2023-11-13 18:56:17 +0100

    Update install_basic

--- src/cmds/sh/html/install_basic.sh
+++ src/cmds/sh/html/install_basic.sh
@@ -9,18 +9,39 @@ mkdir "$folder_name"
 # Aller dans le dossier créé
 cd "$folder_name"
 
-# Créer package.json
+# Initialiser un projet npm
+npm init -y
+
+# Installer les dépendances
+npm install node-sass nodemon babel-cli babel-preset-env webpack webpack-cli --save-dev
+
+
 echo '{
   "name": "'$folder_name'",
   "version": "1.0.0",
   "description": "",
   "main": "index.js",
   "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
+    "css": "node-sass --watch src/scss/main.scss src/css/main.css",
+    "js-babel": "babel src/js/app.js -o src/js/app.min.js --watch --source-maps",
+    "js-webpack": "webpack --watch",
+    "css-js-babel": "npm run css & npm run js-babel",
+    "css-js-webpack": "npm run css & npm run js-webpack",
+    "watch": "nodemon --watch src/scss --watch src/js --ext scss --ext js --exec \"npm run css-js-babel\"",
+    "start": "npm run css & npm run js-babel",
+    "build": "node-sass src/scss/main.scss src/css/main.css && babel src/js/app.js -o src/js/app.min.js --source-maps"
   },
   "keywords": [],
   "author": "",
-  "license": "ISC"
+  "license": "ISC",
+  "devDependencies": {
+    "babel-cli": "^6.26.0",
+    "babel-preset-env": "^1.7.0",
+    "node-sass": "^4.14.1",
+    "nodemon": "^2.0.7",
+    "webpack": "^5.32.0",
+    "webpack-cli": "^4.6.0"
+  }
 }' > package.json
 
 # Créer le dossier src et ses sous-dossiers
@@ -28,6 +49,25 @@ mkdir -p src/css src/scss src/js
 
 # Créer les fichiers css/main.css, scss/main.scss et js/app.js
 touch src/css/main.css src/scss/main.scss src/js/app.js
+
+
+# Créer le fichier .babelrc pour la configuration de Babel
+echo '{
+  "presets": ["env"]
+}' > .babelrc
+
+
+# Créer le fichier webpack.config.js pour la configuration de Webpack
+echo 'const path = require("path");
+
+module.exports = {
+  entry: "./src/js/app.js",
+  output: {
+    filename: "app.min.js",
+    path: path.resolve(__dirname, "src/js")
+  }
+};' > webpack.config.js
+
 
 # Créer index.html avec la base HTML5
 echo '<!DOCTYPE html>
@@ -43,4 +83,9 @@ echo '<!DOCTYPE html>
 </body>
 </html>' > index.html
 
-echo "Installation terminée dans le dossier $folder_name."
+bash npm install
+
+echo "Installation terminée dans le dossier $folder_name. N'oubliez pas d'exécuter 'npm install' pour installer les dépendances si cela n'a pas été fait."
+
+
+
